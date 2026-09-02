79576383bb15a3990570c5a67d12323621e05f4b
Priya Penke <penkepriya05@gmail.com>
2026-09-02 07:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.36%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.py
@@ -0,0 +1,9 @@
+class Solution:
+    def flipAndInvertImage(self, image):
+        for row in image:
+            row.reverse()
+            
+            for i in range(len(row)):
+                row[i] = 1 - row[i]
+        
+        return image
